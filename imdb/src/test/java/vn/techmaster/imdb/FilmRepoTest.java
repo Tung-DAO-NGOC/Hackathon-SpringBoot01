0bfa12d2a6b5d2788c892b01ba50ce248eb96ab6
TungDAO <tungdn30.weo@gmail.com>
2021-11-25 21:49:52 +0700

    Update

--- imdb/src/test/java/vn/techmaster/imdb/FilmRepoTest.java
+++ imdb/src/test/java/vn/techmaster/imdb/FilmRepoTest.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,6 +18,7 @@ import vn.techmaster.imdb.repository.FilmRepository;
 
 @SpringBootTest
 @Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FilmRepoTest {
 	@Autowired
 	private FilmRepository filmRepo;
@@ -25,24 +28,35 @@ class FilmRepoTest {
 	public void getAll() {
 		List<Film> filmList = filmRepo.getAll();
 		log.info("--Testing getAll--");
-		Assertions.assertThat(filmList).hasSize(30);
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(filmList).hasOnlyElementsOfType(Film.class);
+		softAssertions.assertThat(filmList.get(filmList.size() - 1).getId())
+				.isEqualTo(filmList.size());
+		softAssertions.assertAll();
 	}
 
 	@Test
 	@Order(2)
 	public void getFilmByCountry() {
-		Map<String, List<Film>> newMap = filmRepo.getFilmByCountry();
+		Map<String, List<Film>> resultMap = filmRepo.getFilmByCountry();
+		List<Film> filmList = filmRepo.getAll();
+
 		log.info("--Testing getFilmByCountry--");
-		Assertions.assertThat(newMap).hasSize(19);
-	}
+		Assertions.assertThat(resultMap.values().stream().mapToInt(List::size).sum())
+				.isEqualTo(filmList.size());
+	};
 
 	@Test
 	@Order(3)
 	public void getcountryMakeMostFilms() {
 		Entry<String, Integer> resultEntry = filmRepo.getcountryMakeMostFilms();
+		Map<String, List<Film>> resultMap = filmRepo.getFilmByCountry();
 		log.info("--Testing getcountryMakeMostFilms--");
-		log.info(resultEntry.toString());
-		Assertions.assertThat(resultEntry.getKey()).hasToString("China");
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(resultEntry.getValue()).isLessThanOrEqualTo(
+				resultMap.values().stream().mapToInt(List::size).sum());
+		softAssertions.assertThat(resultMap.keySet().contains(resultEntry.getKey())).isTrue();
+		softAssertions.assertAll();
 	}
 
 	@Test
@@ -74,23 +88,18 @@ class FilmRepoTest {
 		Map<String, List<Film>> resultMap = filmRepo.categorizeFilmByGenere();
 		softAssertions.assertThat(resultMap).hasSize(15);
 		softAssertions.assertThat(resultMap).doesNotContainKey("");
-		softAssertions.assertThat(resultMap.get("horror")).hasSize(4);
 		softAssertions.assertAll();
 	}
 
 	@Test
 	@Order(7)
 	public void top5HighMarginFilms() {
-		SoftAssertions softAssertions = new SoftAssertions();
 		List<Film> resultList = filmRepo.top5HighMarginFilms();
 		log.info("--Testing top5HighMarginFilms--");
 		log.info(resultList.get(4).getTitle());
 		log.info(resultList.get(0).getTitle());
-		softAssertions.assertThat(resultList.get(0).getTitle()).hasToString("Resident Evil");
-		softAssertions.assertThat(resultList.get(4).getTitle()).contains("Spring in a Small Town");
-		softAssertions.assertThat(resultList.get(0).getMargin())
+		Assertions.assertThat(resultList.get(0).getMargin())
 				.isGreaterThan(resultList.get(4).getMargin());
-		softAssertions.assertAll();
 	}
 
 	@Test
@@ -111,12 +120,11 @@ class FilmRepoTest {
 	@Test
 	@Order(9)
 	public void ratioBetweenGenere() {
-		SoftAssertions softAssertions = new SoftAssertions();
+		List<String> allGenres = filmRepo.getAllGeneres();
 		log.info("--Testing ratioBetweenGenere--");
-		softAssertions.assertThat(filmRepo.ratioBetweenGenere("adventure", "history"))
-				.isEqualTo(0.875);
-		softAssertions.assertThat(filmRepo.ratioBetweenGenere("science", "horror")).isEqualTo(1);
-		softAssertions.assertAll();
+		Double ratioCheck = filmRepo.ratioBetweenGenere(allGenres.get(0), allGenres.get(1));
+		Double ratioReverse = filmRepo.ratioBetweenGenere(allGenres.get(1), allGenres.get(0));
+		Assertions.assertThat(ratioCheck * ratioReverse).isEqualTo(1d);
 	}
 
 	@Test
